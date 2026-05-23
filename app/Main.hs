248e214300042d377b04d947a9ce64b310bab2db
Daniel Díaz <diaz_carrete@yahoo.com>
2026-05-23 22:30:07 +0200

    ...

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,31 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ImplicitParams #-}
 module Main (main) where
 
-import qualified PrettyException (someFunc)
+import Control.Exception
+import PrettyException (catchAndPrintSomeException)
+import Control.Exception.Backtrace (setBacktraceMechanismState, BacktraceMechanism (IPEBacktrace, HasCallStackBacktrace))
+import GHC.Stack (HasCallStack)
+
+catchAndThrow :: IO a -> IO a
+catchAndThrow action = catch action \(e :: SomeException) -> throwIO e
+
+catchAndRethrow :: IO a -> IO a
+catchAndRethrow action = catchNoPropagate @(ExceptionWithContext SomeException) action rethrowIO
+
+foo :: HasCallStack => IO ()
+foo = do
+  bar 
+
+bar :: HasCallStack => IO  ()
+bar = throwIO (userError "urk")
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  PrettyException.someFunc
+  setBacktraceMechanismState IPEBacktrace True
+  setBacktraceMechanismState HasCallStackBacktrace True
+  putStrLn "# catchAndThrow \n"
+  catchAndPrintSomeException (catchAndThrow foo)
+  putStrLn "\n"
+  putStrLn "# catchAndRethrow \n"
+  catchAndPrintSomeException (catchAndRethrow foo)
--- lib/PrettyException.hs
+++ lib/PrettyException.hs
@@ -1,4 +1,38 @@
-module PrettyException (someFunc) where
+{-# LANGUAGE TypeAbstractions #-}
+module PrettyException (
+    prettyPrintSomeException,
+    catchAndPrintSomeException
+    ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Exception
+import Control.Exception.Backtrace
+import Control.Exception.Context
+import Type.Reflection
+import Data.List (intercalate)
+import Data.Tree (Tree(..), drawTree)
+
+prettyPrintSomeException :: SomeException -> IO ()
+prettyPrintSomeException e = 
+    putStrLn $ drawTree $ someExceptionToTree e 
+
+someExceptionToTree :: SomeException -> Tree String
+someExceptionToTree e = 
+    let context = someExceptionContext e
+        whileHandlings = getExceptionAnnotations @WhileHandling context
+        backtraces = getExceptionAnnotations @Backtraces context
+    in case e of 
+        SomeException @innert _ ->
+            let eRep = typeRep @innert
+             in Node (show $ eRep) (
+                    (whileHandlingToTree <$> whileHandlings)
+                    ++ 
+                    (backtracesToTree <$> backtraces))
+    where 
+    whileHandlingToTree (WhileHandling e') = 
+        Node "WhileHandling" [someExceptionToTree e']
+    backtracesToTree backtraces = 
+        Node ("Backtrace: " ++ (intercalate "/" $ lines $ displayBacktraces backtraces)) []
+
+catchAndPrintSomeException :: IO () -> IO ()
+catchAndPrintSomeException action =
+    catch action prettyPrintSomeException
